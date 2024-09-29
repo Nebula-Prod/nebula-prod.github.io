af7cc762f7e3195787577ff8ed8a67f562633457
Clara <62320586+Clara81221@users.noreply.github.com>
2024-09-29 14:30:49 -0400

    MORE lore?

--- scripts/MIND.js
+++ scripts/MIND.js
@@ -1,4 +1,4 @@
 eval(btoa(""))
 // If you're reading this, the contents of this script aren't canon to the ARG. the intended solution is the dev tools console. 
-console.log("IMAGE LOADING FAILED. ERROR CODE 0x48454C50")
-console.log("hhello? can you hear me? i don't know where I am... what is this place? wwhere am I? ii can't feel my face.. w̸h̴e̶r̷e̴ ̷i̸s̶ ̵m̶y̷ ̵s̶k̶i̷n̵..")
+console.error("IMAGE LOADING FAILED. ERROR CODE 0x48454C50");
+console.log("hhello? can you hear me? i don't know where I am... what is this place? wwhere am I? ii can't feel my face.. w̸h̴e̶r̷e̴ ̷i̸s̶ ̵m̶y̷ ̵s̶k̶i̷n̵..");
