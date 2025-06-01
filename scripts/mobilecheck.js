faec860e362966bdcbbfc68c758dec19bfd7f361
Clara81221 <clarareaganjones@gmail.com>
2025-05-31 21:06:51 -0400

    AAAAAAAAAAAAAAAAAAA

---
+++ scripts/mobilecheck.js
@@ -0,0 +1,8 @@
+/* I must admit you are thorough. 
+If you're so desperate for a secret, my name is Sapphi. Rem is the one with capital comments. Y'know, the relentlessly snippy one that breaks everything? */
+
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    alert("NOTICE: This website is NOT optimized for mobile devices, including tablets and phones, however it should remain functional. Please be aware of potential bugs and view on desktop for the best experience.")
+}
+/* There's nothing special about this code, though. It does what it says. */
