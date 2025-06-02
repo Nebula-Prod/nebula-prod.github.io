016be8a5bb66956df8787c290ee57411709caf80
Clara81221 <clarareaganjones@gmail.com>
2025-06-02 18:24:37 -0400

    Updated social buttons

--- scripts/tilt.js
+++ scripts/tilt.js
@@ -16,10 +16,6 @@ function startTransitionProgress() {
     if (progress < 1) {
       requestAnimationFrame(updateProgress); // Continue until transition completes
     }
-
-    textElements.forEach(text => {
-      text.style.textShadow = `${shadowX * shadowmult}px ${shadowY * shadowmult}px 4px rgba(0, 0, 0, 1)`; // Adjust opacity as needed
-    });
   }
 
   requestAnimationFrame(updateProgress); // Start the progress tracking
@@ -43,32 +39,16 @@ document.querySelectorAll('.projectgriditem').forEach(item => {
 
       const rotateX = (mouseY / itemHeight) * sensitivity; // Properly invert Y-axis rotation
       const rotateY = (mouseX / itemWidth) * sensitivity;   // X-axis rotation remains the same
-
-      // Calculate shadow offsets based on mouse position
-      shadowX = (mouseX / itemWidth) * -30; // Shadow moves in the opposite direction of mouse
-      shadowY = (mouseY / itemHeight) * -30;
       
       theItem = item;
 
       item.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
-      item.style.boxShadow = `${shadowX}px ${shadowY}px 20px rgba(0, 0, 0, 0.6)`; // Dynamic shadow
-
-      // Apply the same shadow to text elements inside the div
-      textElements = item.querySelectorAll('*'); // All child elements
-      textElements.forEach(text => {
-        text.style.textShadow = `${shadowX * shadowmult}px ${shadowY * shadowmult}px 4px rgba(0, 0, 0, 1)`; // Adjust opacity as needed
-      });
+      item.style.boxShadow = `${shadowX}px ${shadowY}px 20px rgba(255, 255, 255, 0.6)`; // Dynamic shadow
     });
   
     item.addEventListener('mouseleave', function() {
       item.style.transform = `rotateX(0deg) rotateY(0deg)`; // Reset rotation on mouse leave
-      item.style.boxShadow = `0 10px 10px rgba(0, 0, 0, 0.4)`; // Reset shadow
-
-      // Reset text shadow when mouse leaves
-      const textElements = item.querySelectorAll('*');
-      textElements.forEach(text => {
-        text.style.textShadow = `0px 1px 2px rgba(0, 0, 0, 0.4)`; // Reset text shadow
-      });
+      item.style.boxShadow = `0 10px 10px rgba(0, 0, 0, 0)`; // Reset shadow
     });
   });
   
